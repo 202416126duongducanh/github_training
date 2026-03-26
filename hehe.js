98f54cbc4b8427ce3c0a467d5e72bca7184914f7
ducanh2006 <duongducanh06@gmail.com>
2026-03-26 14:38:04 +0700

    feat: đang thử tí hheheehheee

--- hehe.js
+++ hehe.js
@@ -1,1 +1,37 @@
-var x = [1,2,3,4];
+import { GrowthBook } from "@growthbook/growthbook";
+
+const growthbook = new GrowthBook({
+  apiHost: "https://cdn.growthbook.io",
+  clientKey: "sdk-QJuFQ7KrCeHOIzJ", // Key của bạn
+  enableDevMode: true,
+  // 1. Bỏ plugins: [ autoAttributesPlugin() ] vì Node.js không hỗ trợ
+  
+  // 2. Tự định nghĩa attributes (Ví dụ: id người dùng để test)
+  attributes: {
+    id: "user-123",
+    company: "HUST",
+    loggedIn: true,
+    role : "adsfa"
+  },
+  
+  trackingCallback: (experiment, result) => {
+    console.log("Viewed Experiment", {
+      experimentId: experiment.key,
+      variationId: result.key
+    });
+  },
+});
+
+// 3. Đợi dữ liệu tải về. Dùng streaming: true để nhận update ngay khi gạt nút
+await growthbook.init({ streaming: true });
+
+console.log("--- KẾT NỐI THÀNH CÔNG ---");
+
+// Test thử xem cờ có ON không
+setInterval(() => {
+    if (growthbook.isOn("feature-1")) {
+        console.log("✅ CỜ ĐANG: ON");
+    } else {
+        console.log("❌ CỜ ĐANG: OFF");
+    }
+}, 3000);
